a38c5e126fe96625b002f6554f9a39c11f2a887c
Enrico Bernz Reichow Santos <71547@PROWAY.TREINA>
2021-12-21 21:39:17 -0300

    commit 2 para teste

--- js/principal.js
+++ js/principal.js
@@ -10,21 +10,21 @@ class Eu {
     }
 
     meApresentar() {
-        return "Olá tenho " + this.idade + " anos, pode entrar o contato pelo email" + this.email + " no momento ja tenho algum conhecimento em: " + this.aprendizado()
+        return "Olá tenho " + this.idade + " anos, pode entrar o contato pelo email" + this.email + " no momento aprendi sobre: <ul> " + this.aprendizado()+"</ul>"
     }
 
     aprendizado() {
         let texto = ""
         for (let contador = 0; contador < this.conhecimentos.length; contador++) {
-            texto += "<br>" + this.conhecimentos[contador]
+            texto += "<li>" + this.conhecimentos[contador]+"</li>"
         }
         return texto
     }
 }
 
 let conhecimentos = [
-    "- Algoritmos 1 (Pseudocódigo com VisualG)",
-    "- Algoritmos 2 (Linguagem de Programação JavaScript"
+    "Algoritmos 1 (Pseudocódigo com VisualG)",
+    "Algoritmos 2 (Linguagem de Programação JavaScript"
 ]
 
 let enrico = new Eu(17, "enricobernzreichow@gmail.com", conhecimentos)
